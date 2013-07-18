30fdaf6ad3ac9eb8eb5958848abff78550c9af6c
webinfluenza <look@webinfluenza.de>
2013-07-18 22:56:59 +0200

    removed jQuery from utils, set window.wiUtils

--- src/wijsutils.js
+++ src/wijsutils.js
@@ -8,88 +8,93 @@
 
 // @TODO: get rid of jQuery, use only vanilla JavaScript
 
+/*jshint unused: true */
 ;( function( $, undefined ) {
-    /**
-     * some global stuff, namespaced
-     **/
-    var wiGlobalSpace = {
-        objectTypes: {
-            '[object Object]': 'object',
-            '[object Array]': 'array',
-            '[object Number]': 'number',
-            '[object String]': 'string'
-        }
-    };
+    var wiUtils = {
+        /**
+         * some global stuff, namespaced
+         **/
+        wiGlobalSpace: {
+            objectTypes: {
+                '[object Object]': 'object',
+                '[object Array]': 'array',
+                '[object Number]': 'number',
+                '[object String]': 'string'
+            }
+        },
 
-    /**
-     * this method splits arrays in X subarrays / chunks
-     **/
-    $.wiChunkArray = function( array, size ) {
-        // is array parameter is actually an array, map into chunks
-        // else return the given object untouched
-        if( array.length ) {
-            // way faster implementation then the old one
-            var results = [];
+        /**
+         * this method splits arrays in X subarrays / chunks
+         **/
+        wiChunkArray: function( array, size ) {
+            // is array parameter is actually an array, map into chunks
+            // else return the given object untouched
+            if( array.length ) {
+                // way faster implementation then the old one
+                var results = [];
 
-            while( array.length ) {
-                results.push( array.splice( 0, size ) );
+                while( array.length ) {
+                    results.push( array.splice( 0, size ) );
+                }
+
+                return results;
+            } else {
+                return array;
+            }
+        },
+
+        /**
+         * check what kind of object we've here and
+         * return the value
+         * @TODO: think about a proper return value, if the parameter was not passed
+         **/
+        wiIsWhat: function( input ) {
+            if( typeof input !== 'undefined' ) {
+                return this.wiGlobalSpace.objectTypes[Object.prototype.toString.call( input )];
+            } else {
+                return "invalid";
+            }
+        },
+
+        /**
+         * Fast DOM element selection by Dustin Diaz
+         * @see http://www.dustindiaz.com/getelementsbyclass/
+         **/
+        wiElementsByClass: function( searchClass, node, tag ) {
+            var classElements = [];
+
+            // if nothing was set, simply return
+            if( typeof searchClass === 'undefined' || !searchClass.length ) {
+                return;
             }
 
-            return results;
-        } else {
-            return array;
-        }
-    };
-
-    /**
-     * check what kind of object we've here and
-     * return the value
-     * @TODO: think about a proper return value, if the parameter was not passed
-     **/
-    $.wiIsWhat = function( input ) {
-        if( typeof input !== 'undefined' ) {
-            return wiGlobalSpace.objectTypes[Object.prototype.toString.call( input )];
-        } else {
-            return "invalid";
-        }
-    };
-
-    /**
-     * Fast DOM element selection by Dustin Diaz
-     * @see http://www.dustindiaz.com/getelementsbyclass/
-     **/
-    $.wiElementsByClass = function( searchClass, node, tag ) {
-        var classElements = [];
-
-        // if nothing was set, simply return
-        if( typeof searchClass === 'undefined' || !searchClass.length ) {
-            return;
-        }
-
-        if( node === null || typeof node === 'undefined' ) {
-            node = document;
-        }
-
-        if( tag === null || typeof tag === 'undefined' ) {
-            tag = '*';
-        }
-
-        if( searchClass.charAt( 0 ) === '.' ) {
-            // remove the trailing .
-            searchClass = searchClass.substr( 1 );
-        }
-
-        var els = node.getElementsByTagName( tag ),
-            elsLen = els.length,
-            pattern = new RegExp( "(^|\\s)" + searchClass + "(\\s|$)" );
-
-        for( var i = 0, j = 0; i < elsLen; i++ ) {
-            if( pattern.test( els[i].className ) ) {
-                classElements[j] = els[i];
-                j++;
+            if( node === null || typeof node === 'undefined' ) {
+                node = document;
             }
-        }
 
-        return classElements;
+            if( tag === null || typeof tag === 'undefined' ) {
+                tag = '*';
+            }
+
+            if( searchClass.charAt( 0 ) === '.' ) {
+                // remove the trailing .
+                searchClass = searchClass.substr( 1 );
+            }
+
+            var els = node.getElementsByTagName( tag ),
+                elsLen = els.length,
+                pattern = new RegExp( "(^|\\s)" + searchClass + "(\\s|$)" );
+
+            for( var i = 0, j = 0; i < elsLen; i++ ) {
+                if( pattern.test( els[i].className ) ) {
+                    classElements[j] = els[i];
+                    j++;
+                }
+            }
+
+            return classElements;
+        }
     };
+
+    window.wiUtils = wiUtils || {};
 }( jQuery ) );
--- test/wijsutils_test.js
+++ test/wijsutils_test.js
@@ -14,10 +14,10 @@
 
     test( 'return splitted Array or unchanged object', 2, function() {
         // valid run with a valid array
-        deepEqual( $.wiChunkArray( this.testArray, 3 ), [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10]], 'Should return an array of 4 arrays' );
+        deepEqual( wiUtils.wiChunkArray( this.testArray, 3 ), [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10]], 'Should return an array of 4 arrays' );
 
         // invalid run with an object instead of an array
-        deepEqual( $.wiChunkArray( this.testObject, 3 ), this.testObject, 'Should return an unchanged object' );
+        deepEqual( wiUtils.wiChunkArray( this.testObject, 3 ), this.testObject, 'Should return an unchanged object' );
     } );
 
     module( 'isWhat', {
@@ -31,10 +31,10 @@
     } );
 
     test( 'test different objects', 4, function() {
-        deepEqual( $.wiIsWhat( this.o ), "object" );
-        deepEqual( $.wiIsWhat( this.a ), "array" );
-        deepEqual( $.wiIsWhat( this.i ), "number" );
-        deepEqual( $.wiIsWhat( this.s ), "string" );
+        deepEqual( wiUtils.wiIsWhat( this.o ), "object" );
+        deepEqual( wiUtils.wiIsWhat( this.a ), "array" );
+        deepEqual( wiUtils.wiIsWhat( this.i ), "number" );
+        deepEqual( wiUtils.wiIsWhat( this.s ), "string" );
     } );
 
     module( 'wiElementsByClass', {
@@ -54,27 +54,27 @@
         var node = document.getElementById( 'qunit-fixture' );
 
         // 1st test: just a class
-        strictEqual( 2, $.wiElementsByClass( 'bar' ).length, 'just a class' );
+        strictEqual( 2, wiUtils.wiElementsByClass( 'bar' ).length, 'just a class' );
 
         // 2nd test: just a class with trailing dot
-        strictEqual( 2, $.wiElementsByClass( '.bar' ).length, 'just a class with trailing dot' );
+        strictEqual( 2, wiUtils.wiElementsByClass( '.bar' ).length, 'just a class with trailing dot' );
 
         // 3rd test: class and node
-        strictEqual( 2, $.wiElementsByClass( 'bar', node ).length, 'class and node' );
+        strictEqual( 2, wiUtils.wiElementsByClass( 'bar', node ).length, 'class and node' );
 
         // 4th test: class with trailing dot and node
-        strictEqual( 2, $.wiElementsByClass( '.bar', node ).length, 'class with trailing dot and node' );
+        strictEqual( 2, wiUtils.wiElementsByClass( '.bar', node ).length, 'class with trailing dot and node' );
 
         // 5th test: class without tailing dot, node and tag
-        strictEqual( 2, $.wiElementsByClass( 'bar', node, 'div' ).length, 'class without tailing dot, node and tag' );
+        strictEqual( 2, wiUtils.wiElementsByClass( 'bar', node, 'div' ).length, 'class without tailing dot, node and tag' );
 
         // 6th test: class with trailing dot, node and tag
-        strictEqual( 2, $.wiElementsByClass( '.bar', node, 'div' ).length, 'class with trailing dot, node and tag' );
+        strictEqual( 2, wiUtils.wiElementsByClass( '.bar', node, 'div' ).length, 'class with trailing dot, node and tag' );
 
         // 7th test: class without trailing dot, without node (null) and with tag
-        strictEqual( 2, $.wiElementsByClass( 'bar', null, 'div' ).length, 'class without trailing dot, without node and with tag' );
+        strictEqual( 2, wiUtils.wiElementsByClass( 'bar', null, 'div' ).length, 'class without trailing dot, without node and with tag' );
 
         // 8th test: class with trailing dot, without node and with tag
-        strictEqual( 2, $.wiElementsByClass( '.bar', null, 'div' ).length, 'class with trailing dot, without node and with tag' );
+        strictEqual( 2, wiUtils.wiElementsByClass( '.bar', null, 'div' ).length, 'class with trailing dot, without node and with tag' );
     } );
 }( jQuery ) );
